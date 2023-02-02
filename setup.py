833374d120f2b15afe8c5861fa4c454dc78489b5
Michael McCloskey <mmccloskey0104@gmail.com>
2023-02-02 15:21:57 -0500

    Added stub software library, see #4

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Smart_Breadboard',
+      version='1.0',
+      description='Smart Breadboard Stub Library',
+      author='Michael McCloskey',
+      author_email='mmccloskey0104@gmail.com',
+      url='https://github.com/wecodewithmichaelmccloskey/Smart_Breadboard',
+      py_modules=['Smart_Breadboard'],
+     )
