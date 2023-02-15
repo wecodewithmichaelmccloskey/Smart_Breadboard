52064177a8bd4536936ca6c2829c84370a0ba868
Fatiha897 <56314544+Fatiha897@users.noreply.github.com>
2023-02-15 13:47:29 -0500

    Added matrix driver dummy function for #5
    nodetoLED
    configureCommandRegister
    turnOnLED
    turnOffLED

--- Smart_Breadboard.py
+++ Smart_Breadboard.py
@@ -4,3 +4,22 @@ def info():
 
 def setupDisplay():
     pass
+
+
+
+# Matrix Driver functions
+#takes a "node" number on the physical circuit and returns the address of the port on the matrix driver that will be used to turn on/off the corresponding LED
+def nodeToLED (): 
+    pass
+
+# configures command register in order to select for LED control register
+def configureCommandRegister ():
+    pass 
+
+# turn off LED by sending byte to port address
+def turnOnLED (portAdd):
+    pass
+
+#turn on LED by sending byte to port address
+def turnOffLED (portAdd):
+    pass
