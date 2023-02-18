ecbd3b4bd54a8642447f453447d015d54940718f
Michael McCloskey <mmccloskey0104@gmail.com>
2023-02-18 13:04:01 -0500

    Added dummy functions for controlling display, see #12

--- Smart_Breadboard.py
+++ Smart_Breadboard.py
@@ -2,10 +2,22 @@ def info():
     '''Prints a basic library description'''
     print("Software library for the Smart Breadboard project.")
 
+# LCD Touchscreen Display functions
+# initializes display settings
 def setupDisplay():
     pass
 
+# clears the display so that the screen only shows a white background
+def clearDisplay():
+    pass
 
+# sends text to the display at a specified location
+def sendTextToDisplay():
+    pass
+
+# sends an image to the display at a specified location
+def sendImageToDisplay():
+    pass
 
 # Matrix Driver functions
 #takes a "node" number on the physical circuit and returns the address of the port on the matrix driver that will be used to turn on/off the corresponding LED
