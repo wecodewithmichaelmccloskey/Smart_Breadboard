fdafc85d6fa255c1c7bdba740ae234a414f00e50
Fatiha897 <56314544+Fatiha897@users.noreply.github.com>
2023-02-22 13:56:42 -0500

    Added working code for matrix driver function #7
    runLEDDebug, turnOffAll added with working code
    
    configureCommandRegister deleted (not necessary)
    
    nodeToLED, turnOnLED, turnOffLED has working code

--- Smart_Breadboard.py
+++ Smart_Breadboard.py
@@ -1,3 +1,22 @@
+#Matrix driver imports + global variables
+import board
+import busio
+from adafruit_is31fl3731.matrix import Matrix as Display
+
+i2c = busio.I2C(board.SCL, board.SDA)
+display = Display(i2c)
+
+#Matrix and Matrix A LED Control Register Address
+MTRX_DRV_ADDR = 0x74
+CA1 = 0x00
+CA2 = 0x02
+CA3 = 0x04
+CA4 = 0x06
+CA5 = 0x08
+CA6 = 0x0A
+CA7 = 0x0C
+CA8 = 0x0E
+CA9 = 0x10
 def info():  
     '''Prints a basic library description'''
     print("Software library for the Smart Breadboard project.")
@@ -21,20 +40,46 @@ def sendImageToDisplay():
 
 # Matrix Driver functions
 #takes a "node" number on the physical circuit and returns the address of the port on the matrix driver that will be used to turn on/off the corresponding LED
-def nodeToLED (): 
-    pass
-
-# configures command register in order to select for LED control register
-def configureCommandRegister ():
-    pass 
+def nodeToLED(LEDID):
+   if LEDID == 1:
+      return CA1
+   elif LEDID == 2:
+      return CA2
+   elif LEDID == 3:
+       return CA3
+   elif LEDID == 4:             
+      return CA4
+   elif LEDID == 5:
+       return CA5
+   elif LEDID == 6:
+       return CA6
+   elif LEDID == 7:
+        return CA7
+   elif LEDID == 8:
+        return CA8
+   elif LEDID == 9:
+        return CA9
+   else:
+        return "Value not in Matrix range" 
 
 # turn off LED by sending byte to port address
 def turnOnLED (portAdd):
-    pass
+      display.pixel(pix, 0, 127)
 
 #turn on LED by sending byte to port address
 def turnOffLED (portAdd):
-    pass
+     display.pixel(pix, 0, 0)
+
+#turns off all LEDs in the matrix
+def turnOffAll():
+  display.fill(0)
+
+ # takes in an array of LEDs identfied to be turned on, and another array of LEDs identified to be turned off and performs on/off
+def runLEDDebug(turnOnArr, turnOffArr):
+  for i in turnOnArr:
+    turnOnLED(nodeToLED(i))
+  for j in turnOffArr:
+    turnOffLED(nodeToLED(j))
 
 #Current Sensor functions
 #reading current given proper parameters
