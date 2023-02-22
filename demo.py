12886bc251eb442465f988394bc31441b873290c
Fatiha897 <56314544+Fatiha897@users.noreply.github.com>
2023-02-22 13:54:22 -0500

    Updated Matrix Driver functions for demo.py #7

--- demo.py
+++ demo.py
@@ -1,26 +1,5 @@
 import Smart_Breadboard
 
-#Matrix driver imports + global variables
-import board
-import busio
-from adafruit_is31fl3731.matrix import Matrix as Display
-
-i2c = busio.I2C(board.SCL, board.SDA)
-display = Display(i2c)
-
-#Matrix and Matrix A LED Control Register Address
-MTRX_DRV_ADDR = 0x74
-CA1 = 0x00
-CA2 = 0x02
-CA3 = 0x04
-CA4 = 0x06
-CA5 = 0x08
-CA6 = 0x0A
-CA7 = 0x0C
-CA8 = 0x0E
-CA9 = 0x10
-
-
 portAdd = None
 
 Smart_Breadboard.info()
@@ -40,47 +19,19 @@ Smart_Breadboard.sendImageToDisplay()
 
 # Matrix Driver functions
 #takes a "node" number on the physical circuit and returns the address of the port on the matrix drive>
-Smart_Breadboard.nodeToLED(LEDID):
-   if LEDID == 1:
-      return CA1
-   elif LEDID == 2:
-      return CA2
-   elif LEDID == 3:
-       return CA3
-   elif LEDID == 4:             
-      return CA4
-   elif LEDID == 5:
-       return CA5
-   elif LEDID == 6:
-       return CA6
-   elif LEDID == 7:
-        return CA7
-   elif LEDID == 8:
-        return CA8
-   elif LEDID == 9:
-        return CA9
-   else:
-        return "Value not in Matrix range"
+Smart_Breadboard.nodeToLED(LEDID)
 
 # turn off LED by sending byte to port address
-Smart_Breadboard.turnOnLED(pix):
-  display.pixel(pix, 0, 127)
+Smart_Breadboard.turnOnLED(pix)
  
-
 #turn on LED by sending byte to port address
-Smart_Breadboard.turnOffLED(pix):
-  display.pixel(pix, 0, 0)
+Smart_Breadboard.turnOffLED(pix)
 
  #turns off all LEDs in the matrix
-Smart_Breadboard.turnOffAll():
-  display.fill(0)
+Smart_Breadboard.turnOffAll()
 
  # takes in an array of LEDs identfied to be turned on, and another array of LEDs identified to be turned off and performs on/off
-Smart_Breadboard.runLEDDebug(turnOnArr, turnOffArr):
-  for i in turnOnArr:
-    turnOnLED(nodeToLED(i))
-  for j in turnOffArr:
-    turnOffLED(nodeToLED(j))
+Smart_Breadboard.runLEDDebug(turnOnArr, turnOffArr)
 
 #Current Sensor functions
 #reading current given proper parameters
